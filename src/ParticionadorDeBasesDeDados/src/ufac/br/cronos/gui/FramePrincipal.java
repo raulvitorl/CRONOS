dd0c241382c8fdca1f5e5f0f9c422eb38a51aa3d
Raul Vitor L <raulawliet@gmail.com>
2018-11-22 19:19:00 -0500

    Atualização
    Realizei o enpacotamento correto do código para faclitar na compreensão do mesmo, foram criadas as cama de lógica e de camada de interface.

---
+++ src/ParticionadorDeBasesDeDados/src/ufac/br/cronos/gui/FramePrincipal.java
@@ -0,0 +1,127 @@
+package ufac.br.cronos.gui;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+
+import ufac.br.cronos.logic.Escolhedor;
+
+public class FramePrincipal extends JFrame implements ActionListener{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -961040415780285993L;
+
+	static  String caminho,separador;
+
+	static int inicio;
+
+	Escolhedor Es = new Escolhedor();
+
+	JButton btnBusca = new JButton();
+	JButton btnConfirma = new JButton();
+	JPanel pnlBusca,pnlMetricas,pnlIntervalo,pnlRepartir;
+	JComboBox<String> listaMetricas = new JComboBox<>();	
+
+	//Vetores para armazenar as unidades e intervalos
+	JComboBox<String> listaDeUnidades = new JComboBox<String>();
+
+	JComboBox<String> listaDeDuracao = new JComboBox<String>();
+
+	ButtonGroup bgFiltros;
+
+
+
+	public FramePrincipal(){
+		super("Particionador de Bases de Dados");
+		this.setBackground(new Color(245));
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(500, 550);
+		this.setResizable(false);
+		this.setLayout(new GridLayout(7, 1));
+		pnlBusca = new JPanel();
+		btnBusca.setText("Buscar Base");
+		btnConfirma.setText("Confirmar Divisão");
+		pnlBusca.add(btnBusca);
+		pnlMetricas = new JPanel();
+		pnlRepartir = new JPanel();
+		pnlRepartir.setLayout(new GridLayout(1, 3));
+		pnlRepartir.add(new JLabel(""));
+		pnlRepartir.add(btnConfirma);
+		pnlRepartir.add(new JLabel(""));
+		pnlMetricas.add(listaMetricas);
+		pnlIntervalo = new JPanel();
+		pnlIntervalo.setLayout(new GridLayout(4, 2));
+
+		pnlIntervalo.add(new JLabel("Unidade : "));
+		pnlIntervalo.add(listaDeUnidades);
+		listaDeUnidades.addItem("Dia(s)");
+		listaDeUnidades.addItem("Mês(es)");
+		listaDeUnidades.addItem("Ano(s)");
+		pnlIntervalo.add(new JLabel("Duracao : "));		
+		pnlIntervalo.add(listaDeDuracao);
+
+		this.add(pnlBusca);
+		this.add(new JLabel("                                   "
+				+ "                                      Atributos"));
+		this.add(pnlMetricas);
+		this.add(new JLabel("                                                  "
+				+ "                       Intervalo"));
+
+		this.add(pnlIntervalo);
+		this.add(pnlRepartir);
+		btnBusca.addActionListener(this);
+		btnConfirma.addActionListener(this);
+		listaDeUnidades.addActionListener(this);
+		listaMetricas.addActionListener(this);
+		setLocationRelativeTo(null);
+		setVisible(true); 
+
+	}
+
+
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("com.jtattoo.plaf.luna.LunaLookAndFeel");
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		FramePrincipal fp = new FramePrincipal();
+		fp.setVisible(true);
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==btnBusca){
+			caminho = Es.EscolheArquivo();
+			if(caminho!="") {
+				inicio = Integer.parseInt(JOptionPane.showInputDialog(null,"Em que linha iniciam os dados do arquivo?"));
+				separador = JOptionPane.showInputDialog(null,"Qual o simbolo separador do arquivo?");
+				boolean success = Es.PreencheMetricas(listaMetricas, separador, caminho, inicio);
+				if(!success){JOptionPane.showMessageDialog(null,"Falha na captura do arquivo");}
+			}else{
+				JOptionPane.showMessageDialog(null,"Selecione um arquivo!");
+			}
+		}
+		if(e.getSource()==btnConfirma){
+			
+		}
+		if(e.getSource()==listaDeUnidades){
+			Es.DefineIntervalo(listaDeUnidades.getSelectedItem().toString(), listaDeDuracao);
+		}
+	}
+
+
+}
---
+++ src/ParticionadorDeBasesDeDados/src/ufac/br/cronos/logic/Escolhedor.java
@@ -0,0 +1,215 @@
+package ufac.br.cronos.logic;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+public class Escolhedor{
+
+
+	@SuppressWarnings("resource")
+	public boolean PreencheMetricas(JComboBox<String> listaMetricas,String separador,String caminho,int inicio){
+		listaMetricas.removeAllItems();			
+		JFileChooser escolheArquivo = new JFileChooser();
+		FileNameExtensionFilter filtro = new FileNameExtensionFilter("*csv", "csv");
+		escolheArquivo.setFileFilter(filtro);
+		//int resposta = escolheArquivo.showOpenDialog(new JDialog());
+			File file = new File(caminho);			 
+			InputStream is = null;
+			try {
+				is = new FileInputStream(file);
+			} catch (FileNotFoundException e1) {
+				return false;
+			}
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			String s = null;
+			int cont = 0,atributos=0;
+			String[] textoSeparado= {};
+			try {
+				s = br.readLine();
+			} catch (IOException e1) {
+				return false;
+			}
+			while (s != null) {
+				textoSeparado=s.split(separador);
+				cont++;
+				if(cont==inicio){
+					atributos = textoSeparado.length;
+					for(int i=0;i<atributos;i++){
+						try {
+							listaMetricas.addItem(textoSeparado[i]);
+						} catch (NullPointerException npe) {
+							npe.printStackTrace();
+						}
+
+					}
+				}				
+
+				try {
+					s = br.readLine();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+			try {
+				br.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		
+		return true;
+
+	}
+	
+	public boolean Particiona(String primeiraData,String segundaData,int coluna,String caminhoFinal,String formato,String ArquivoDeOrigem,String separador) throws Exception{
+		Date d1 = formataData(primeiraData,formato);
+		Date d2 = formataData(segundaData,formato);
+		File file = new File(ArquivoDeOrigem);
+		InputStream is = null;
+		File file2 = new File(caminhoFinal);
+		OutputStream os = null;
+		try {
+			os = new FileOutputStream(file2, true);
+		} catch (FileNotFoundException e2) {e2.printStackTrace();}
+		try {
+			os = new FileOutputStream(file2, true);
+		} catch (FileNotFoundException e2) {e2.printStackTrace();}
+		OutputStreamWriter osw = new OutputStreamWriter(os);
+		try {
+			is = new FileInputStream(file);
+		} catch (FileNotFoundException e1) {e1.printStackTrace();}
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		String s = null;
+		int cont = 0;
+		String[] textoSeparado= {};
+		try {
+			s = br.readLine();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		while (s != null) {
+			textoSeparado=s.split(separador);
+			cont++;
+			//Escrevendo o cabeçalho
+			if(cont==1){
+				for(int i=0;i<textoSeparado.length;i++){
+					if(i==textoSeparado.length){
+						osw.write(textoSeparado[i]);
+					}
+					if(i!=textoSeparado.length){
+						osw.write(textoSeparado[i]+",");
+					}
+
+				}
+				osw.write("\r\n");
+			}
+			if(cont>1){
+				Date temp = null;
+				try {
+					try {
+
+				temp = formataData(textoSeparado[coluna],formato);
+					} catch (ParseException pe) {return false;}
+					if(temp.after(d1) && temp.before(d2)){	
+						try {
+							for(int i=0;i<textoSeparado.length;i++){
+								System.out.println(i);
+								if(i==textoSeparado.length){
+									osw.write(textoSeparado[i]);
+								}
+								if(i!=textoSeparado.length){
+									osw.write(textoSeparado[i]+",");
+								}
+
+							}
+							osw.write("\r\n");
+						} catch (IOException e){e.printStackTrace();}
+					}
+				} catch (NullPointerException npe) {JOptionPane.showMessageDialog(null, "Linha de inicio dos dados incorreta!");
+				return  false;
+				}
+			}
+			try {
+				s = br.readLine();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}	
+		try {
+			osw.close();
+			br.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		return true;
+	}
+	
+	public String EscolheArquivo(){
+		JFileChooser escolheArquivo = new JFileChooser();
+		FileNameExtensionFilter filtro = new FileNameExtensionFilter("*csv", "csv");
+		escolheArquivo.setFileFilter(filtro);
+		int resposta = escolheArquivo.showOpenDialog(new JDialog());
+		String caminhofinal = "";
+		if (resposta == JFileChooser.APPROVE_OPTION) {
+			caminhofinal = escolheArquivo.getSelectedFile().getPath().toString();
+		}
+		//JOptionPane.showMessageDialog(null, "Efetuando Partição", "Aguarde", JOptionPane.INFORMATION_MESSAGE, icon);
+		/*try {
+			GeraIntervalo(listaDeUnidades.getSelectedItem().toString(), listaDeDuracao.getSelectedItem().toString(),listaMetricas.getSelectedIndex(),caminhofinal);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
+		return caminhofinal;
+	}
+	
+	public boolean DefineIntervalo(String tipoIntervalo,JComboBox<String> listaDeDuracao){
+		listaDeDuracao.removeAllItems();
+		if(tipoIntervalo.equals("Dia(s)")){
+			for(int i=1;i<30;i++){
+				listaDeDuracao.addItem(Integer.toString(i));
+			}
+		}
+		if(tipoIntervalo.equals("Mês(es)")){
+			for(int i=1;i<12;i++){
+				listaDeDuracao.addItem(Integer.toString(i));
+			}
+		}
+		if(tipoIntervalo.equals("Ano(s)")){
+			for(int i=1;i<11;i++){
+				listaDeDuracao.addItem(Integer.toString(i));
+			}
+		}
+		return true;
+	}
+	
+	public static java.sql.Date formataData(String data,String formato) throws Exception { 
+		if (data == null || data.equals(""))
+			return null;
+		java.sql.Date date = null;
+		try {
+			//DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy'T'HH:mm:ss'Z'");
+			DateFormat formatter = new SimpleDateFormat(formato);
+			date = new java.sql.Date( ((java.util.Date)formatter.parse(data)).getTime() );
+		} catch (ParseException e){            
+
+		}
+		return date;
+	}
+}
